d4d17c14940db7cde937429e2ccb993c03379d53
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-05-20 13:31:48 -0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,127 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link';
+
+export default function Home() {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
+
+
+
+
+
+
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.main__content}>
+        <div className={styles.form__container}>
+          <Image
+            width={135}
+            height={32}
+            src="images/logo.svg"
+            alt="logo"
+            priority={true}
+            className={styles.form__logo}
+          />
+          <div className={styles.form__logo_container}>
+            <Image
+              width={91}
+              height={63}
+              src="images/logo-mobile.svg"
+              alt="logo"
+              priority={true}
+              className={styles.form__logo_mobile}
+            />
+          </div>
+          <form action="" className={styles.form} onClick={onSubmit}>
+            <div className={styles.form__title_container}>
+              <h2 className={styles.form__title}>NFT Access</h2>
+              <p className={styles.form__subtitle}>
+                Please fill your detail to access your account.
+              </p>
+            </div>
+
+            <div>
+              <div className={styles.form__input_container}>
+                <div className={styles.form__group}>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className={styles.placeholder__input}
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="debra.holt@example.com"
+                  />
+                </div>
+
+                <div className={styles.form__group}>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    className={styles.placeholder__input}
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="••••••••"
+                  />
+                </div>
+              </div>
+
+              <div className={styles.form__group_checkbox}>
+                <div className={styles.form__checkbox}>
+                  <input type="checkbox" id="checkbox" name="remember" />
+                  <label htmlFor="checkbox">Remember me</label>
+                </div>
+                <Link href="/">Forgot Password?</Link>
+              </div>
+
+              <div className={styles.form__buttons_container}>
+                <button className={styles.form__signin} aria-label="Sign in">
+                  Sign in
+                </button>
+                <button
+                  className={styles.form__google}
+                  aria-label="Sign in with Google"
+                >
+                  Sign in with Google
+                </button>
+              </div>
+
+              <p> </p>
+              <div className={styles.form__down}>
+                <p>Don’t have an account?</p>
+                <div className={styles.form__signup}>
+                  <Link href="/">Sign up</Link>
+                </div>
+              </div>
+            </div>
+
+            <Image
+              width={135}
+              height={32}
+              src="images/barra-footer.svg"
+              alt="barra del footer"
+              priority={true}
+              className={styles.form__barra}
+            />
+          </form>
+        </div>
+
+        <div className={styles.form__image}>
+          <Image
+            width={768}
+            height={896}
+            priority={true}
+            src="images/image.svg"
+            alt="logo"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
